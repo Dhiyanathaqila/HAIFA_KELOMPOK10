6b87326974d075b5c617b0f02ffe1560e57ebef1
Dhiyanathaqila <haifaqilla@gmail.com>
2025-10-04 08:54:31 +0700

    Menambahkan project Modul 4 (Java, Python, PHP, C++)

---
+++ MODUL4JAVA_KEL10/Modul4Java_Kel10.java
@@ -0,0 +1,48 @@
+package com.example;
+public class Modul4Java_Kel10 {
+
+        //non-return type dengan parameter
+        static void cetakKelompok(String kelompok) {
+            System.out.println(kelompok);
+        }
+        //non-return type tanpa parameter
+        static void cetakHello() {
+            System.out.println("Halo Praktikan Pemrograman Dasar 2025 ^^");
+        }
+        //return type dengan parameter
+        static String returnParam(String text) {
+            return text;
+        }
+        //return type tanpa parameter
+        static String returnText() {
+            return "Hari ini kita akan belajar mengenai Function dan Method";
+        }
+        public static void main(String[] args) {
+            //penggunaan function non-return type dengan mengisi argument pada parameter kelompok
+            cetakKelompok("Kelompok 10");
+            //penggunaan function non-return type tanpa parameter
+            cetakHello();
+            //penggunaan function return type dengan mengisi argument pada parameter
+            System.out.println(returnParam ("Ini merupakan function return type"));
+
+            //penggunaan function return type tanpa parameter
+            System.out.println(returnText());
+            System.out.println(" ");
+
+
+            ContohClass objek = new ContohClass();
+
+
+            objek.menyapa();
+
+
+            String printPerkenalan = objek.perkenalan("Haifa", "Semarang", "Membuat Kreatif Manik-Manik");
+            System.out.println(printPerkenalan);
+
+
+            objek.printUmur(17);
+        }
+
+    }
+
+
---
+++ MODUL4PY_KEL10/call_method.py
@@ -0,0 +1,7 @@
+from percobaan2_kel10 import method
+
+obj = method("Budi", 21120125120001)
+
+obj.intro()
+
+obj.timer(5)
